d6a2de87b9a0e64f2fed4582a6e263c1239b365e
ksen61 <kspankova13@gmail.com>
2024-02-17 23:52:35 +0300

    pract2
    test

---
+++ Class1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace pract3
+{
+    public class Note
+    {
+        public int id;
+        public DateTime date;
+        public string title;
+        public string desc;
+
+        public Note(int id, string title, string desc, DateTime date)
+        {
+            this.title = title;
+            this.desc = desc;
+            this.date = date;
+            this.id = id;
+        }
+    }
+
+    public class Schedule
+    {
+        public readonly List<Note> todayNotes;
+        public readonly List<Note> allNotes;
+        public DateTime selectedDate;
+        public int selectedId = -1;
+
+        public Schedule(DateTime date)
+        {
+            todayNotes = Data.LoadNotes(date);
+            allNotes = Data.LoadNotes();
+            selectedDate = date;
+        }
+
+        public void RefreshNotes()
+        {
+            todayNotes.Clear();
+            todayNotes.AddRange(Data.LoadNotes(selectedDate));
+        }
+
+        public void UpdateNotes()
+        {
+            Data.SaveNotes(allNotes);
+        }
+
+        public void NewNote(string title, string description, DateTime date)
+        {
+            int id = allNotes.Count > 0 ? allNotes[allNotes.Count - 1].id + 1 : 0; 
+            Note note = new Note(id, title, description, date);
+            allNotes.Add(note);
+            UpdateNotes();
+            RefreshNotes();
+        }
+
+        public void EditNote(string title, string description, DateTime date)
+        {
+            if (selectedId != -1 && selectedId < todayNotes.Count)
+            {
+                Note note = new Note(todayNotes[selectedId].id, title, description, date);
+                DeleteNote();
+                allNotes.Add(note);
+                UpdateNotes();
+                RefreshNotes();
+                selectedId = -1;
+            }
+        }
+
+        public void DeleteNote()
+        {
+            if (selectedId != -1 && selectedId < todayNotes.Count)
+            {
+                int id = todayNotes[selectedId].id;
+                allNotes.RemoveAll(note => note.id == id);
+                RefreshNotes();
+                UpdateNotes();
+            }
+        }
+    }
+
+    public class Data
+    {
+        private const string notesFilePath = "notes.json";
+
+        public static void SaveNotes(List<Note> notes)
+        {
+            try
+            {
+                File.WriteAllText(notesFilePath, JsonConvert.SerializeObject(notes));
+            }
+            catch 
+            { 
+               
+            }
+        }
+
+        public static List<Note> LoadNotes(DateTime date = default)
+        {
+            List<Note> notes;
+            try
+            {
+                string jsonData = File.ReadAllText(notesFilePath);
+                notes = JsonConvert.DeserializeObject<List<Note>>(jsonData);
+
+                if (date != default)
+                {
+                    notes = notes.FindAll(note => note.date == date);
+                }
+            }
+            catch
+            {
+                notes = new List<Note>();
+                SaveNotes(notes);
+            }
+
+            return notes;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace pract3
+{
+    public partial class MainWindow : Window
+    {
+        public static Schedule schedule;
+        public static DateTime selectedDate = DateTime.Today;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            schedule = new Schedule(selectedDate);
+            UpdateList();
+            dateContainer.SelectedDate = selectedDate;
+        }
+
+        public void UpdateList()
+        {
+            selectedDate = schedule.selectedDate;
+            TaskContainer.Items.Clear();
+            schedule.RefreshNotes();
+            foreach (Note note in schedule.todayNotes)
+            {
+                TaskContainer.Items.Add(note.title);
+            }
+            titleBox.Text = "";
+            descriptionBox.Text = "";
+        }
+
+        private void CreateButtonClick(object sender, RoutedEventArgs e)
+        {
+            string title = titleBox.Text;
+            string description = descriptionBox.Text;
+            schedule.NewNote(title, description, selectedDate);
+            UpdateList();
+        }
+
+        private void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            string title = titleBox.Text;
+            string description = descriptionBox.Text;
+            schedule.EditNote(title, description, selectedDate);
+            UpdateList();
+        }
+
+        private void ChangeSelect(object sender, SelectionChangedEventArgs e)
+        {
+            if (TaskContainer.SelectedIndex != -1)
+            {
+                schedule.selectedId = TaskContainer.SelectedIndex;
+                Note selectedNote = schedule.todayNotes[schedule.selectedId];
+                titleBox.Text = selectedNote.title;
+                descriptionBox.Text = selectedNote.desc;
+            }
+        }
+
+        private void SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (DateTime.TryParse(dateContainer.Text, out DateTime selectedDate))
+                {
+                    schedule.selectedDate = selectedDate;
+                    UpdateList();
+                }
+            }
+            catch 
+            {
+                
+            }
+        }
+
+        private void DeleteButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (schedule.selectedId != -1)
+            {
+                schedule.DeleteNote();
+                UpdateList();
+            }
+        }
+    }
+}
